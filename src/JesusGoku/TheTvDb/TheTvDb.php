8409d3d23e24ffe23c36046cbcbd26bb660ff413
Jesus Urrutia <jesus.urrutia@gmail.com>
2015-06-13 20:37:32 -0300

    Added search method

--- src/JesusGoku/TheTvDb/TheTvDb.php
+++ src/JesusGoku/TheTvDb/TheTvDb.php
@@ -15,13 +15,44 @@ class TheTvDb
     /** @var string */
     private $apiKey;
 
+    /** @var string */
+    private $baseUrl = 'http://thetvdb.com/api/';
+
+    /** @var string */
+    private $language = 'en';
+
     /** @var Client */
     private $client;
 
-    public function __construct($apiKey)
+    public function __construct($apiKey, array $config = array())
     {
         $this->apiKey = $apiKey;
 
-        $this->client = new Client();
+        if (isset($config['baseUrl'])) $this->baseUrl = $config['baseUrl'];
+        if (isset($config['language'])) $this->language = $config['language'];
+
+        var_dump($this->baseUrl);
+        $this->client = new Client(array(
+            'base_uri' => $this->baseUrl,
+        ));
+    }
+
+    public function search($q)
+    {
+        $res = $this->client->get('http://thetvdb.com/api/GetSeries.php', array(
+            'query' => array(
+                'seriesname' => $q,
+                'language' => $this->language,
+            ),
+        ));
+
+        $xml = simplexml_load_string((string) $res->getBody());
+
+        $tvShows = array();
+        foreach ($xml->Series as $serie) {
+            $tvShows[] = new TvShow($serie);
+        }
+
+        return $tvShows;
     }
 }
