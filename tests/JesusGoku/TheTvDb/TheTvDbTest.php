aca05b223477777b9a7515abf9cbeb702e7f08dd
Jesus Urrutia <jesus.urrutia@gmail.com>
2015-06-14 20:02:10 -0300

    Complete tests

--- tests/JesusGoku/TheTvDb/TheTvDbTest.php
+++ tests/JesusGoku/TheTvDb/TheTvDbTest.php
@@ -3,7 +3,9 @@
 namespace JesusGoku\TheTvDb;
 
 use JesusGoku\TheTvDb\TheTvDb;
-use JesusGoku\TheTvDb\Language;
+use JesusGoku\TheTvDb\Models\Language;
+use JesusGoku\TheTvDb\Models\Banner;
+use JesusGoku\TheTvDb\Models\Actor;
 
 /**
  * Class TheTvDbTest
@@ -30,10 +32,66 @@ class TheTvDbTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('247808', $tvShows[0]->getId());
     }
 
+    public function testTvShowByImdbId()
+    {
+        $tvShow = $this->api->getTvShowByImdbId('tt0944947');
+
+        $this->assertEquals(121361, $tvShow->getId());
+    }
+
     public function testLanguages()
     {
         $languages = $this->api->getLanguages();
 
         $this->assertTrue($languages[0] instanceof Language);
+    }
+
+    public function testBanners()
+    {
+        $banners = $this->api->getBanners(121361);
+
+        $this->assertTrue($banners[0] instanceof Banner);
+    }
+
+    public function testTvShow()
+    {
+        $tvShow = $this->api->getTvShow(121361);
+
+        $this->assertEquals(121361, $tvShow->getId());
+    }
+
+    public function testActors()
+    {
+        $actors = $this->api->getActors(121361);
+
+        $this->assertTrue($actors[0] instanceof Actor);
+    }
+
+    public function testEpisode()
+    {
+        $episode = $this->api->getEpisode(3254641);
+
+        $this->assertEquals(3254641, $episode->getId());
+    }
+
+    public function testEpisodeByDefault()
+    {
+        $episode = $this->api->getEpisodeByDefault(121361, 1, 1);
+
+        $this->assertEquals(3254641, $episode->getId());
+    }
+
+    public function testEpisodeByDvd()
+    {
+        $episode = $this->api->getEpisodeByDvd(121361, 1, 1);
+
+        $this->assertEquals(3254641, $episode->getId());
+    }
+
+    public function testEpisodeByAbsolute()
+    {
+        $episode = $this->api->getEpisodeByAbsolute(121361, 1);
+
+        $this->assertEquals(3254641, $episode->getId());
     }
 }
